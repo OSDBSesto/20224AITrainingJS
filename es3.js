2096bb697bc614d6a4034eed1577b145d418ddf5
mattia.pogliani <mattia.pogliani@gmail.com>
2023-03-09 09:27:49 +0100

    edit es3.js wit api server call

--- es3.js
+++ es3.js
@@ -1,7 +1,55 @@
 var count = 2;
+var n1, n2;
 $("#btn1").click(function () {
-  calculation("+");
+  if ($("#N1").val() && $("#N2").val()) {
+    n1 = +$("#N1").val();
+    n2 = +$("#N2").val();
+    somma(n1, n2);
+  } else alert("fill all the text field");
 });
+
+function somma(a, b) {
+  console.log("somma", a, b);
+  // chiama api somma
+  $.ajax("http://localhost:3000/somma?a=" + a + "&b=" + b)
+    .done(function (data) {
+      console.log("success:", data);
+      // chiama la funzione di renderizzazione
+      renderizzazione(a, b, "+", data);
+    })
+    .fail(function (err) {
+      console.log("error:", err);
+    });
+}
+
+function renderizzazione(n1, n2, sign, res) {
+  console.log(
+    "strings:",
+    localStorage.getItem("trStrings")
+  );
+  count++;
+  // crea la riga della tabella
+  var trString =
+    "<tr><td>" +
+    count +
+    "</td><td>" +
+    n1 +
+    "</td><td>" +
+    sign +
+    "</td><td>" +
+    n2 +
+    "</td><td>" +
+    res +
+    "</td></tr>";
+  // la aggiunge al local storage
+  localStorage.setItem(
+    "trStrings",
+    localStorage.getItem("trStrings") + trString
+  );
+  // la renderizza nella tabella
+  $("#table").append(trString);
+}
+
 $("#btn2").click(function () {
   calculation("-");
 });
@@ -12,49 +60,9 @@ $("#btn4").click(function () {
   calculation("/");
 });
 
-var addFirstLine = (function() {
+var addFirstLine = (function () {
   if (localStorage.getItem("trStrings")) {
     console.log("bella");
     $("#table").append(localStorage.getItem("trStrings"));
   }
 })();
-
-function calculation(sign) {
-  console.log("strings:", localStorage.getItem("trStrings"));
-  if ($("#N1").val() && $("#N2").val()) {
-    count++;
-    var n1 = +$("#N1").val();
-    var n2 = +$("#N2").val();
-    var res = n1 + n2;
-    switch (sign) {
-      case "+":
-        var res = n1 + n2;
-        // n1 -(-n2) ;
-        break;
-      case "-":
-        var res = n1 - n2;
-        break;
-      case "*":
-        var res = n1 * n2;
-        break;
-      case "/":
-        var res = n1 / n2;
-        break;
-    }
-    console.log(res);
-    var trString =
-      "<tr><td>" +
-      count +
-      "</td><td>" +
-      n1 +
-      "</td><td>" +
-      sign +
-      "</td><td>" +
-      n2 +
-      "</td><td>" +
-      res +
-      "</td></tr>";
-    localStorage.setItem("trStrings", localStorage.getItem("trStrings")+trString);
-    $("#table").append(trString);
-  } else alert("fill all the text field");
-}
